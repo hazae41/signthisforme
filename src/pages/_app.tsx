cb2fc7a7ad2c4d93fe454f6bf4bca19a6bdc7227
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-08-31 12:01:17 +0700

    manifest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -35,13 +35,13 @@ export default function App({ Component, pageProps }: AppProps) {
     <Head>
       <title>SignThisFor.Me</title>
       <meta key="viewport" name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover" />
-      <meta key="application-name" name="application-name" content="Brume Wallet" />
-      <meta key="description" name="description" content="The private wallet" />
+      <meta key="application-name" name="application-name" content="SignThisFor.Me" />
+      <meta key="description" name="description" content="Sign This For Me" />
       <meta key="color-scheme" name="color-scheme" content="dark light" />
       <meta key="theme-color-light" name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
       <meta key="theme-color-dark" name="theme-color" content="#000000" media="(prefers-color-scheme: dark)" />
       <meta key="apple-mobile-web-app-capable" name="apple-mobile-web-app-capable" content="yes" />
-      <meta key="apple-mobile-web-app-title" name="apple-mobile-web-app-title" content="Brume Wallet" />
+      <meta key="apple-mobile-web-app-title" name="apple-mobile-web-app-title" content="SignThisFor.Me" />
       <meta key="apple-mobile-web-app-status-bar-style" name="apple-mobile-web-app-status-bar-style" content="white" />
       <meta key="referrer" name="referrer" content="no-referrer" />
       <link rel="shortcut icon" href="/favicon.png" />
